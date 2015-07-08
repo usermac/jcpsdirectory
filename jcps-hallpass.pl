d1b8dc791fa1ff50bf9f3b40386a301136a9797d
BrianGinn <brian.ginn@jefferson.kyschools.us>
2015-07-08 16:13:01 -0400

    PERL program done. The initial program to convert the Etherpad text-only export of Directory works for the web search!. - Brian

--- jcps-hallpass.pl
+++ jcps-hallpass.pl
@@ -1,22 +1,18 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-#150707 - Is YYMMDD for documentation purposes. - Brian
-#150707 - See GitHub usermac/jcpsdirectory for more about this project. - Brian
-print "Hello World\n";
-my $animal = "<!-- bear -->"; 
-print "The animal is a $animal.\n";
-my $bananas = "";
-print "We have no bananas.\n" unless $bananas;
-#150707 - You should name the input file "directory-input.txt" and put is same dir. - Brian
-open(my $in, "<", "directory-input.txt") or die "Can't open directory-input.txt:$!";
-open(my $out, ">", "directory-output.txt") or die "Can't open directory-output.txt:$!";
-open(my $log, ">>", "directory-log.txt") or die "Can't open directory-log.txt:$!";
+# 150707 - Is YYMMDD for documentation purposes. - Brian
+# 150707 - See GitHub usermac/jcpsdirectory for more about this project. - Brian
+open(my $in, "<", "directory-input.txt") or die "Can't open directory-input.txt:$!"; # 150708 - Rename your exported Etherpad text-only export to this name. - Brian
+open(my $out, ">", "directory-output.txt") or die "Can't open directory-output.txt:$!"; # 150708 - This is the results file to be used on the Web. - Brian
+open(my $log, ">>", "directory-log.txt") or die "Can't open directory-log.txt:$!"; # 150708 - Future use. It may become the log file. - Brian
 while(<$in>){ #assigns each line in turn to $_
-  s/Aldus/RAMOS/;
-  s/text/TEXT/;
-  s/<!--/\t/;
-  s/(\S)-->/$1 -->/;
-    print "Just read in this line: $_\n";
-  }
-  
+my $mark = /RWB|HEADING/; #150708 - If within the html comments of the Etherpad text-only export it finds RWB for Red, White and Blue for schools or HEADING for department heads mark it. - Brian
+  if ($mark) { # 150708 - This will be 1 or blank. See above. - Brian
+    print $out "<li><h2 class=\"name\">$_</h2>\n"; # 150708 - It is a school name or department heading so print with html trappings of h2. - Brian
+    } else {
+    print $out "<li><h3 class=\"name\">$_</h3>\n"; # 150708 - It is not a school name or department heading so print with html trappings of h3. - Brian
+    }
+  }    
+my $time = localtime; # scalar context. Needed to setup the freshness date and time at the top. - Brian
+print $out "\n<li><h2 class=\"edited\">Updated: $time<!--  --></h2>\n"; # 150708 - This is it. How it writes each line to the file. - Brian
