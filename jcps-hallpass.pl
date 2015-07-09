510dafcbc6fbddb2b6a8650fa643e746c7097683
BrianGinn <brian.ginn@jefferson.kyschools.us>
2015-07-09 13:58:01 -0400

    Changed some filenames. Added credit var and put at top of listing out and in timestamp html files. - Brian

--- jcps-hallpass.pl
+++ jcps-hallpass.pl
@@ -2,11 +2,12 @@
 use strict;
 use warnings;
 # 150707 - Is YYMMDD for documentation purposes. - Brian
-# 150707 - See GitHub usermac/jcpsdirectory for more about this project. - Brian
+my $credit = "<!-- 7/9/2015 - See GitHub usermac/jcpsdirectory for more about this project. - Brian. -->"; # 150709 - Please do not remove. - Brian
 open(my $in, "<", "directory-input.txt") or die "Can't open directory-input.txt:$!"; # 150708 - Rename your exported Etherpad text-only export to this name. - Brian
-open(my $edited, ">", "directory-edited-timestamp.html") or die "Can't open directory-edited-timestamp.html:$!"; # 150709 - This simply writes to a file the edited timestamp. This is for Mike because I saw this on footer of Web pages - Brian
+open(my $edited, ">", "directory-timestamp.html") or die "Can't open directory-timestamp.html:$!"; # 150709 - This simply writes to a file the edited timestamp. This is for Mike because I saw this on footer of Web pages - Brian
 open(my $out, ">", "directory-output-admin.html") or die "Can't open directory-output-admin.html:$!"; # 150708 - This is the results file to be used on the Web. - Brian
 open(my $log, ">>", "directory-log.txt") or die "Can't open directory-log.txt:$!"; # 150708 - Future use. It may become the log file. - Brian
+print $out "$credit\n"; # 150708 - Write credit to the top of the file. - Brian
 while(<$in>){ #assigns each line in turn to $_
   s/\n//; # 150709 - Remove the line ending as I'll add them back after the html code. This is just for beauty and has no function. - Brian
   s/jefferson\.kyschools\.us//; #150709 - Remove the domain name why? Because it becomes a mess when using the web pages' search. Remove it. - Brian
@@ -17,5 +18,5 @@ while(<$in>){ #assigns each line in turn to $_
       print $out "<li><h3 class=\"name\">$_</h3>\n"; # 150708 - It is not a school name or department heading, it's a person, so print with html trappings of h3. - Brian
     }
   }    
-my $time = localtime; # scalar context. Needed to setup the freshness date and time at the top. - Brian
-print $edited "Updated: $time<!--  -->"; # 150708 - Write the timestamp to a file for later linking with the update information. - Brian
+my $time = localtime; # scalar context. Needed to setup the freshness date and time of the data. - Brian
+print $edited "$time  $credit"; # 150708 - Write the timestamp to a file for later linking with the update information. - Brian
