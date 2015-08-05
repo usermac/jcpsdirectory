ef35cc71b82a48a84ce0c6454599b33ca53c05a6
BrianGinn <brian.ginn@jefferson.kyschools.us>
2015-08-05 14:53:36 -0400

    v1.0b as shown to Mike for the meeting today. It has the 2,400 people processed in alpha order but uncovered lots of formatting errors in the Etherpad data. Half I fixed, the other half I'll have to do next ( no time before the meeting, I just edited the .js file). - Brian

--- jcps-hallpass.pl
+++ jcps-hallpass.pl
@@ -42,9 +42,14 @@ open(my $in2, "<", "directory-input-sorted.txt") or die "Can't open directory-in
 open(my $json_people, ">", "directory-people.js") or die "Can't open directory-people.js:$!"; # 目录 150708—This is the results file to be used on the Web for JSON read-only.—Brian
 open(my $json_schools, ">", "directory-schools.js") or die "Can't open directory-schools.js:$!"; # 红白蓝 150726—This is the results file to be used on the Web for JSON read-only.—Brian
       print $json_schools "{\n    \"school\":\n    [\n    {\n"; # 红白蓝 150730—This is the opening lines to the JSON.—Brian
+      print $json_people "{\n    \"people\":\n    [\n    {\n"; # 目录 150805—This is the opening lines to the JSON.—Brian
 my $address = "\",\n        \"address\": \""; # 红白蓝 150803—JSON between code for address text.—Brian
+my $position = "\",\n        \"position\": \""; # 目录 150805—JSON between code for position text.—Brian
+my $loc_name = "\",\n        \"loc_name\": \""; # 目录 150805—JSON between code for location name text.—Brian
 my $phone = "\",\n        \"phone\": \"(502)"; # 红白蓝 150803—JSON between code for phone text.—Brian
 my $fax = "\",,\n        \"fax\": \"(502)"; # 红白蓝 150803—JSON between code for fax text.—Brian
+my $school_department = "\",\n        \"school/department\": \""; # 目录 150805—JSON between code for school or department name text.—Brian
+my $email = "\",\n        \"email\": \""; # 目录 150805—JSON between code for school or department name text.—Brian
 my $meta = "\",,\n        \"meta\": \""; # 红白蓝 150803—JSON between code for meta text.—Brian
 while(<$in2>){ # 红白蓝 assigns each line in turn to $_
   s/\n//; # 红白蓝 150709—Remove the line ending as I'll add them back after the html code. This is just for beauty and has no function.—Brian
@@ -70,11 +75,27 @@ while(<$in2>){ # 红白蓝 assigns each line in turn to $_
       # 150804—Do nothing as I can't get the not-if statment correct so I do it this way. Sheesh.—Brian
      } else  { # 目录 150804—Now, finally, write the person to the file. It needs to be made JSON but it finds the right people now. ^_^ —Brian
        my $count = () = $_ =~ /\;/g; # 目录 150805—Counts number of semicolons. If 5 it's a department otherwise it's 4 and is a school. If it's not either 4 or 5, it's a line item that needs correcting at the Etherpad. This accounts for the building number just before phone in departments only.—Brian
-       print $json_people "$count";
-       print $json_people "<li><h3 class=\"name\">$_</h3>\n"; # 目录 150803—YET TO BE PROGRAMMED It is not a school name or department heading; it's a person, so print with JSON trappings.—Brian
+#       print $json_people "$count";
+#       print $json_people "<li><h3 class=\"name\">$_</h3>\n"; # 目录 150803—YET TO BE PROGRAMMED It is not a school name or department heading; it's a person, so print with JSON trappings.—Brian
       # 150805—Sample output from this PERL script follows:
       # *** coming attraction ***
+      s/, 4/, Louisville KY 4/; # 红白蓝 150803—Fills in the assumed city and state of "Louisville, KY" for each ", 4" which is the end of the address and beginning of the ZIP code.—Brian
+      s/;/$position/; # 红白蓝 150803—The in-between code from the var above goes here.—Brian
+      if ($count == 5) {
+        s/; /$loc_name/; # 红白蓝 150803—The in-between code from the var above goes here.—Brian
+      }
+      s/; /$phone/; # 红白蓝 150803—The in-between code from the var above goes here.—Brian
+      s/• /$fax/; # 红白蓝 150803—The in-between code from the var above goes here.—Brian
+      s/; /$school_department/; # 目录 150805—The in-between code from the var above goes here.—Brian
+      s/; /$email/; # 目录 150805—The in-between code from the var above goes here.—Brian
+      s/" /"/; # 红白蓝 150803—Fix a silly formatting error where I don't account for a space properly above.—Brian
+      s/<!-- /$meta/; # 红白蓝 150803—The in-between code from the var above goes here.—Brian
+      s/ -->/"/; # 红白蓝 150803—Replace the close comment tag with a simple quote to end the entry.—Brian
+      s/ ",/"/; # 红白蓝 150803—Fix more silly formatting error where I don't account for a space properly above.—Brian
+      s/ ",/"/; # 红白蓝 150803—[Again] Fix more silly formatting error where I don't account for a space properly above.—Brian
+      s/,,/,/; # 目录 150805—[Again and again] Fix more silly formatting error where I don't account for a space properly above.—Brian
+      s/,,/,/; # 目录 150805—[Again and again] Fix more silly formatting error where I don't account for a space properly above.—Brian
+      print $json_people "        \"entity\": \"$_\n    }, {\n"; # 红白蓝 150708—It is a school name or department heading, so print with html trappings of h2.—Brian      
        } 
      }
-print $json_schools "        \"comment\": \"end of file. $credit\"\n    }\n    ]\n}"; # 红白蓝 150730—This is the closing lines to the JSON.—Brian
-
+for my $print ($json_schools, $json_people) { print $print "        \"comment\": \"end of file. $credit\"\n    }\n    ]\n}"}; # 红白蓝 目录 150805—This is the closing lines to the JSON.—Brian;
